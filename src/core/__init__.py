a1493349fb1f4c4ea4384d9c58cf00b13d6f2041
UITxWoodyNguyen <woodydadrillers0707@gmail.com>
2026-08-10 19:54:40 +0700

    .

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -11,10 +11,28 @@ from src.core.pe_parser import (
     PEInfo,
     PECheckResult,
 )
+from src.core.string_extractor import (
+    ExtractedString,
+    IOCMatch,
+    StringExtractor,
+    extract_strings,
+    extracted_strings,
+)
 
 __all__ = [
     "calculate_hash",
     "HashError",
+    "parse_pe_import",
+    "parse_pe_info",
+    "print_imports",
+    "ImportedFunction",
+    "ImportedDLL",
+    "PEInfo",
     "PE_checker",
     "PECheckResult"
+    "ExtractedString",
+    "IOCMatch",
+    "StringExtractor",
+    "extract_strings",
+    "extracted_strings",
 ]
